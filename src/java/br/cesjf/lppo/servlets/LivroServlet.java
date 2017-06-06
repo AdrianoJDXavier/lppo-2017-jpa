fd96968347de26ab18ea7fdbaad57d2b17630237
alunoces <alunoces@L5MAQ10.labcesac.net>
2017-06-05 21:57:45 -0300

    Move o listar para o mesmo servlet.

--- src/java/br/cesjf/lppo/servlets/LivroServlet.java
+++ src/java/br/cesjf/lppo/servlets/LivroServlet.java
@@ -1,9 +1,10 @@
-
 package br.cesjf.lppo.servlets;
 
 import br.cesjf.lppo.Livro;
 import br.cesjf.lppo.dao.LivroJpaController;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import javax.annotation.Resource;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceUnit;
@@ -18,70 +19,82 @@ import javax.transaction.UserTransaction;
  *
  * @author alunoces
  */
-@WebServlet(name = "LivroServlet", urlPatterns = {"/editar.html", "/excluir.html"})
+@WebServlet(name = "LivroServlet", urlPatterns = {"/editar.html", "/excluir.html", "listar.html"})
 public class LivroServlet extends HttpServlet {
+
     @PersistenceUnit(unitName = "lppo-2017-jpa@PU")
     EntityManagerFactory emf;
-    
+
     @Resource(name = "java:comp/UserTransaction")
     UserTransaction ut;
-    
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        if(request.getServletPath().contains("/editar.html")){
-            doEditarGet(request,response);
-        }else if(request.getServletPath().contains("/excluir.html")){
-            doExcluirGet(request,response);
-            
+        if (request.getServletPath().contains("/editar.html")) {
+            doEditarGet(request, response);
+        } else if (request.getServletPath().contains("/excluir.html")) {
+            doExcluirGet(request, response);
+
+        } else if (request.getServletPath().contains("/listar.html")) {
+            doListarGet(request, response);
+
         }
     }
 
-    
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        if(request.getServletPath().contains("/editar.html")){
-            doEditarPost(request,response);
+        if (request.getServletPath().contains("/editar.html")) {
+            doEditarPost(request, response);
         }
     }
 
     private void doEditarGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        try{
-        LivroJpaController dao = new LivroJpaController(ut, emf);
-        Long id = Long.parseLong(request.getParameter("id"));
-        Livro livro = dao.findLivro(id);
-        request.setAttribute("livro", livro);
-        request.getRequestDispatcher("WEB-INF/livro-editar.jsp").forward(request, response);
-    }catch(Exception e){
-        response.sendRedirect("listar.html");
-}
-    }
-    
-    private void doEditarPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        try{
-        LivroJpaController dao = new LivroJpaController(ut, emf);
-        Long id = Long.parseLong(request.getParameter("id"));
-        Livro livro = dao.findLivro(id);
-        livro.setTitulo(request.getParameter("titulo"));
-        livro.setAutor(request.getParameter("autor"));
-        livro.setAno(Integer.parseInt(request.getParameter("ano")));
-        dao.edit(livro);
-        response.sendRedirect("listar.html");
-        
-    }catch(Exception e){
-        response.sendRedirect("listar.html");
-}
+        try {
+            LivroJpaController dao = new LivroJpaController(ut, emf);
+            Long id = Long.parseLong(request.getParameter("id"));
+            Livro livro = dao.findLivro(id);
+            request.setAttribute("livro", livro);
+            request.getRequestDispatcher("WEB-INF/livro-editar.jsp").forward(request, response);
+        } catch (Exception e) {
+            response.sendRedirect("listar.html");
+        }
     }
 
-   
+    private void doEditarPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            LivroJpaController dao = new LivroJpaController(ut, emf);
+            Long id = Long.parseLong(request.getParameter("id"));
+            Livro livro = dao.findLivro(id);
+            livro.setTitulo(request.getParameter("titulo"));
+            livro.setAutor(request.getParameter("autor"));
+            livro.setAno(Integer.parseInt(request.getParameter("ano")));
+            dao.edit(livro);
+            response.sendRedirect("listar.html");
+
+        } catch (Exception e) {
+            response.sendRedirect("listar.html");
+        }
+    }
+
     private void doExcluirGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        try{
-        LivroJpaController dao = new LivroJpaController(ut, emf);
-        Long id = Long.parseLong(request.getParameter("id"));
-        dao.destroy(id);
-        }catch(Exception ex){            
+        try {
+            LivroJpaController dao = new LivroJpaController(ut, emf);
+            Long id = Long.parseLong(request.getParameter("id"));
+            dao.destroy(id);
+        } catch (Exception ex) {
         }
         response.sendRedirect("listar.html");
-    }   
+    }
+
+    private void doListarGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<Livro> livros;
+
+        LivroJpaController dao = new LivroJpaController(ut, emf);
+        livros = dao.findLivroEntities();
+
+        request.setAttribute("livros", livros);
+        request.getRequestDispatcher("WEB-INF/listar-livros.jsp").forward(request, response);
+    }
 }
